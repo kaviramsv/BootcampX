26d4b7897cf7aa1f77b6f92bf778cf367083bad5
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-11-22 23:58:20 +0000

    Add joins and related files

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,3 +1,7 @@
+-- The VAR in VARCHAR means that you can set the max size to anything between 1 and 65,535.
+-- TEXT fields have a fixed max size of 65,535 characters. A VARCHAR can be part of an index
+-- whereas a TEXT field requires you to specify a prefix length, which can be part of an index.
+
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
